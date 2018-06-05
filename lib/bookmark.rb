1077d18b1bae2fedebdaea05752e3de67c585946
Robert Faldo <robertfaldo@gmail.com>
2018-06-05 16:13:25 +0100

    up to step 8 before I add anything

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,10 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      "http://makersacademy.com",
-      "http://destroyallsoftware.com",
-      "http://google.com"
-     ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map { |bookmark| bookmark['url'] }
   end
-
 end
