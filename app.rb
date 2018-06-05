d0c170b1e017cd7ed5277d99f7c5fe8760a15d13
Robert Faldo <robertfaldo@gmail.com>
2018-06-05 16:13:25 +0100

    ste §p to step 8 before I add anything

--- app.rb
+++ app.rb
@@ -1,20 +1,11 @@
 require 'sinatra/base'
-require './lib/bookmark'
+require_relative './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello, worldy!'
-  end
-
   get '/bookmarks' do
-    @bookmarks = [
-      "http://makersacademy.com",
-      "http://destroyallsoftware.com",
-      "http://google.com",
-    ]
+    @bookmarks = Bookmark.all
     erb :index
   end
 
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
