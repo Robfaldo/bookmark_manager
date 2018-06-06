a9682e75dee34b951647ef3ac1fae98b08a93283
Robert Faldo <robertfaldo@gmail.com>
2018-06-06 18:27:56 +0100

    refactored add_bookmark_spec

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -4,34 +4,33 @@
 
 feature 'Adding bookmarks' do
   scenario 'a user can add bookmarks' do
-    # Setup
-    visit('/bookmarks')
-    # Exercise
-    fill_in('bookmark', with: 'http://addbookmark.com')
-    click_button('submit')
-    # Verify
-    expect(page).to have_content "http://addbookmark.com"
+    visit_bookmarks_and_submit_valid_website
+    expect(page).to have_content "http://google.com"
   end
   scenario 'a user can add multiple bookmarks' do
-    # Setup
-    visit('/bookmarks')
-    # Exercise
-    fill_in('bookmark', with: 'http://addbookmark.com')
-    click_button('submit')
+    visit_bookmarks_and_submit_valid_website
     fill_in('bookmark', with: 'http://secondeample.com')
     click_button('submit')
-    # Verify
-    expect(page).to have_content "http://addbookmark.com"
+    expect(page).to have_content "http://google.com"
     expect(page).to have_content "http://secondeample.com"
   end
 
   scenario 'user enters invalid url' do
-    visit('/bookmarks')
-    # Exercise
-    fill_in('bookmark', with: 'addbookmark')
-    click_button('submit')
+    visit_bookmarks_and_submit_invalid_website
     expect(page).to have_content 'you must submit a valid url'
     expect(page).not_to have_content "addbookmark"
+  end
+
+  def visit_bookmarks_and_submit_valid_website
+    visit('/bookmarks')
+    fill_in('bookmark', with: 'http://google.com')
+    click_button('submit')
+  end
+
+  def visit_bookmarks_and_submit_invalid_website
+    visit('/bookmarks')
+    fill_in('bookmark', with: 'invalid website')
+    click_button('submit')
   end
 
 end
