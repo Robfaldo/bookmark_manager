1d2094b54dd8fc55fcc0760148f9efaff9c030da
Robert Faldo <robertfaldo@gmail.com>
2018-06-07 18:32:15 +0100

    fixed the typo in delete_bookmark_spec

--- spec/features/delete_bookmark_spec.rb
+++ spec/features/delete_bookmark_spec.rb
@@ -3,6 +3,6 @@ feature 'Deleting Bookmarks' do
     bookmark = Bookmark.create(url: "http://www.website.com", title: "Website")
     visit('/bookmarks')
     click_button "delete-#{bookmark.id}"
-    expect(page).not_to have_content('Google')
+    expect(page).not_to have_content('Website')
   end
 end
